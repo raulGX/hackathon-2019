03f0ed6cb91cbd8eec965ac3216c070431636936
Bogdan Ailincai <bogdan.ailincai@caspar.ai>
2019-10-05 17:46:01 +0300

    mock events, show cards

--- frontend/src/model/reducers/events.js
+++ frontend/src/model/reducers/events.js
@@ -1,7 +1,14 @@
 import createReducer from 'model/store/createReducer';
 
 function FETCH_EVENTS_SUCCESS(nextState, { result }) {
-  nextState.entities = result;
+  nextState.entities =
+    result.length > 0
+      ? result
+      : [
+          { name: 'KHHASHIDA', description: 'dsdsa asd d das das das das' },
+          { name: 'dgffd', description: 'fsdfs asd d das das das das' },
+          { name: 'dsfsfds', description: 'dsdsa sfsdfsf d das das das das' }
+        ];
   nextState.isLoading = false;
 }
 
--- frontend/src/pages/Events/Events.js
+++ frontend/src/pages/Events/Events.js
@@ -1,6 +1,11 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect, useDispatch } from 'react-redux';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 import { fetchEvents } from 'model/actions/events';
 
@@ -11,7 +16,30 @@ function Events({ events }) {
     dispatch(fetchEvents());
   }, [dispatch]);
 
-  return <>{events}</>;
+  return (
+    <>
+      {events.map(event => (
+        <Card key={event.name}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Word of the Day
+            </Typography>
+            <Typography variant="h5" component="h2">
+              be nev lent
+            </Typography>
+            <Typography color="textSecondary">adjective</Typography>
+            <Typography variant="body2" component="p">
+              well meaning and kindly.
+              <br />a benevolent smile
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      ))}
+    </>
+  );
 }
 
 Events.propTypes = {
