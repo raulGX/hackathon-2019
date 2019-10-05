04b2f79fbcbd788a68ead0ad88209748eaa44875
Bogdan Mihai <bogdan.mihai@bytex.ro>
2019-10-05 16:14:11 +0300

    Delete localhost alias

--- frontend/devtools/webpack/dev.js
+++ frontend/devtools/webpack/dev.js
@@ -4,7 +4,6 @@ let common = require('./common.js');
 const devConfig = merge(common, {
   devServer: {
     port: 3000,
-    host: 'local.huddldev.com',
     disableHostCheck: true,
     historyApiFallback: true,
     hot: true
