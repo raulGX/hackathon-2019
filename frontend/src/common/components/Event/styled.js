ebe7d4622a41443d86754e37f3689cb73206230b
Bogdan Ailincai <bogdan.ailincai@caspar.ai>
2019-10-05 23:20:59 +0300

    Event component

---
+++ frontend/src/common/components/Event/styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const StyledEvent = styled.div`
+  display: flex;
+  align-content: center;
+  align-items: center;
+  justify-content: space-between;
+`;
