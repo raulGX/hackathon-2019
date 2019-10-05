8338db7d7c31b2af5893cf4f546b04c482ced53b
rapopovi <rapopovi@cisco.com>
2019-10-05 17:14:15 +0300

    added cors executable

--- backend/main.go
+++ backend/main.go
@@ -33,6 +33,9 @@ func main() {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		if (*req).Method == "OPTIONS" {
+			return
+		}
 		next(w, req)
 	})
 
