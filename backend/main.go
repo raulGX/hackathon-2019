594635e92de63406931f69f4b949be75e514d124
rapopovi <rapopovi@cisco.com>
2019-10-05 18:50:35 +0300

    token fix

--- backend/main.go
+++ backend/main.go
@@ -38,7 +38,7 @@ func main() {
 	n.UseFunc(func(w http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-TOKEN")
 		if (*req).Method == "OPTIONS" {
 			return
 		}
