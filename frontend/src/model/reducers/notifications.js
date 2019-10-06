cf993bf9ba40e7f09210a157262c5feff7291026
Bogdan Ailincai <bogdan.ailincai@caspar.ai>
2019-10-06 08:39:54 +0300

    notify on event end

--- frontend/src/model/reducers/notifications.js
+++ frontend/src/model/reducers/notifications.js
@@ -1,3 +1,4 @@
+import uuidv1 from 'uuid/v1';
 import createReducer from 'model/store/createReducer';
 
 function SHOW_SNACKBAR(nextState, { notification }) {
@@ -8,11 +9,26 @@ function REMOVE_SNACKBAR(nextState, { key }) {
   nextState.list = nextState.list.filter(notif => notif.key !== key);
 }
 
+// show notification shen event ends - received notification
+function MODIFY_EVENT(nextState, { payload }) {
+  if (payload.finished) {
+    nextState.list.push({
+      key: uuidv1(),
+      message: `Event ${payload.name} has ended. You have received ${payload.rewardCredits} coins.`,
+      options: {
+        variant: 'success',
+        persist: true
+      }
+    });
+  }
+}
+
 const reducer = createReducer(
   { list: [] },
   {
     SHOW_SNACKBAR,
-    REMOVE_SNACKBAR
+    REMOVE_SNACKBAR,
+    '@@backend/EVENT_STATUS_MODIFIED': MODIFY_EVENT
   }
 );
 
