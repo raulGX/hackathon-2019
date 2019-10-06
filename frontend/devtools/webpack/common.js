423e16579375a2835c9ebbaccefe934884b661b5
Bogdan Mihai <bogdan.mihai@bytex.ro>
2019-10-06 09:28:59 +0300

    Added design for event page

--- frontend/devtools/webpack/common.js
+++ frontend/devtools/webpack/common.js
@@ -40,6 +40,14 @@ const options = {
     rules: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      },
+      {
         test: /\.js$/,
         loader: 'eslint-loader',
         exclude: /node_modules/,
